53d455520a38f6140895623bd34e075ac29297d1
Christel <christel.ehrhart@ce-soft.info>
2025-01-08 15:12:42 +0100

    Page blog - articles dynamiques Factorisation des articles dans un nouveau fichier (include)

--- blog.php
+++ blog.php
@@ -9,6 +9,15 @@
 	include_once("header.php");
 	
 	require_once("connexion.php");
+	
+	$strQuery		= "SELECT articles.*, CONCAT(user_name, \" \", user_firstname) 
+						AS \"user_name\"
+						FROM articles
+							INNER JOIN users ON article_creator = user_id
+						ORDER BY article_createdate DESC
+						";
+	$arrArticles	= $db->query($strQuery)->fetchAll();
+	
 ?>
 
 	<div class="row mb-2">
@@ -37,61 +46,11 @@
 			</fieldset>
 		</form>
 	
-		<article class="col-md-6">
-			<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-				<div class="col p-4 d-flex flex-column position-static">
-					<h3 class="mb-0">LE devenir du Javascript </h3>
-					<div class="mb-1 text-body-secondary">11/05/2017 (test)</div>
-					<p class="mb-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit... </p>
-					<a href="#" class="icon-link gap-1 icon-link-hover stretched-link">Lire la suite</a>
-				</div>
-				<div class="col-auto d-none d-lg-block">
-					<img class="bd-placeholder-img" width="200" height="250" alt="js" src="assets/images/js.png">
-				</div>
-			</div>
-		</article>
-		
-		<article class="col-md-6">
-			<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-				<div class="col p-4 d-flex flex-column position-static">
-					<h3 class="mb-0">Qu'est-ce que le HTML?</h3>
-					<div class="mb-1 text-body-secondary">04/04/2017 (christel)</div>
-					<p class="mb-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit... </p>
-					<a href="#" class="icon-link gap-1 icon-link-hover stretched-link">Lire la suite</a>
-				</div>
-				<div class="col-auto d-none d-lg-block">
-					<img class="bd-placeholder-img" width="200" height="250" alt="html" src="assets/images/html.png">
-				</div>
-			</div>
-		</article>
-
-		<article class="col-md-6">
-			<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-				<div class="col p-4 d-flex flex-column position-static">
-					<h3 class="mb-0">Utiliser le CSS correctement</h3>
-					<div class="mb-1 text-body-secondary">08/05/2017 (christel)</div>
-					<p class="mb-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit... </p>
-					<a href="#" class="icon-link gap-1 icon-link-hover stretched-link">Lire la suite</a>
-				</div>
-				<div class="col-auto d-none d-lg-block">
-					<img class="bd-placeholder-img" width="200" height="250" alt="css" src="assets/images/CSS.png">
-				</div>
-			</div>
-		</article>
-
-		<article class="col-md-6">
-			<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-				<div class="col p-4 d-flex flex-column position-static">
-					<h3 class="mb-0">Utiliser PhpMyAdmin</h3>
-					<div class="mb-1 text-body-secondary">21/05/2017 (christel)</div>
-					<p class="mb-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit... </p>
-					<a href="#" class="icon-link gap-1 icon-link-hover stretched-link">Lire la suite</a>
-				</div>
-				<div class="col-auto d-none d-lg-block">
-					<img class="bd-placeholder-img" width="200" height="250" alt="mysql" src="assets/images/mysql.png">
-				</div>
-			</div>
-		</article>				
+	<?php
+		foreach($arrArticles as $arrDetArticle){
+			include("article.php");
+		}
+	?>
 	</div>
 <?php
 	include_once("footer.php");
