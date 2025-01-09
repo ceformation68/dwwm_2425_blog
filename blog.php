2f5abf545f2cd2ebec2361bb88268f27ad7a3cfe
Christel <christel.ehrhart@ce-soft.info>
2025-01-09 11:40:47 +0100

    Recherches : - par mot clé - par date exacte - par période de date
    Dynamisation du mot clé de filtre (WHERE / AND)

--- blog.php
+++ blog.php
@@ -10,12 +10,44 @@
 	
 	require_once("connexion.php");
 	
-	$strQuery		= "SELECT articles.*, CONCAT(user_name, \" \", user_firstname) 
-						AS \"user_name\"
+	var_dump($_POST);
+	// Récupération des données du formulaire
+	$strKeywords 	= $_POST['keywords']??"";
+	$strDate 		= $_POST['date']??"";
+	$strStartDate 	= $_POST['startdate']??"";
+	$strEndDate 	= $_POST['enddate']??"";
+	$intPeriod		= $_POST['period']??"";
+	
+	$strQuery		= "SELECT articles.*, 
+						CONCAT(user_name, \" \", user_firstname) AS \"user_name\"
 						FROM articles
-							INNER JOIN users ON article_creator = user_id
-						ORDER BY article_createdate DESC
-						";
+							INNER JOIN users ON article_creator = user_id";
+	// Définition du mot clé de condition
+	$strWhere		= " WHERE ";
+	// Recherche par mots clés
+	if ($strKeywords != ""){
+		$strQuery	.= $strWhere." (article_title LIKE '%".$strKeywords."%' 
+							OR article_content LIKE '%".$strKeywords."%') ";
+		$strWhere	= " AND "; // un seul where possible => And
+	}		
+							
+	// Recherche par date exacte
+	if ($intPeriod == 0 && $strDate != ""){
+		$strQuery	.= $strWhere." article_createdate = '".$strDate."'";
+		$strWhere	= " AND "; // un seul where possible => And
+	}		
+							
+	// Recherche par période de dates
+	if ($intPeriod == 1 && $strStartDate != "" && $strEndDate != ""){
+		$strQuery	.= $strWhere." article_createdate 
+								BETWEEN '".$strStartDate."' AND '".$strEndDate."'";
+		$strWhere	= " AND "; // un seul where possible => And
+	}		
+							
+	$strQuery		.= " ORDER BY article_createdate DESC;";
+						
+						
+	var_dump($strQuery);
 	$arrArticles	= $db->query($strQuery)->fetchAll();
 	
 ?>
@@ -24,16 +56,23 @@
 		<form name="formSearch" method="post" action="#">
 			<fieldset>
 				<legend>Rechercher des articles</legend>
-				<p><label for="keywords">Mots clés</label><input id="keywords" type="text" name="keywords" /></p>
-				<p>	<input type="radio" name="period" checked value="0" onclick="changePeriod()" /> Par date exacte
+				<p>
+					<label for="keywords">Mots clés</label>
+					<input id="keywords" type="text" name="keywords" value="<?php echo($strKeywords); ?>" />
+				</p>
+				<p>	
+					<input type="radio" name="period" checked value="0" onclick="changePeriod()" /> Par date exacte
 					<input type="radio" name="period" value="1" onclick="changePeriod()" /> Par période
 				</p>
 				<p id="uniquedate">
-					<label for="date">Date</label><input id="date" type="date" name="date" />
+					<label for="date">Date</label>
+					<input id="date" type="date" name="date" value="<?php echo($strDate); ?>" />
 				</p>
 				<p id="period">
-					<label for="startdate">Date de début</label><input id="startdate" type="date" name="startdate" />
-					<label for="enddate">Date de fin</label><input id="enddate" type="date" name="enddate" />
+					<label for="startdate">Date de début</label>
+					<input id="startdate" type="date" name="startdate" value="<?php echo($strStartDate); ?>" />
+					<label for="enddate">Date de fin</label>
+					<input id="enddate" type="date" name="enddate" value="<?php echo($strEndDate); ?>"/>
 				</p>
 				<p>
 					<label for="author">Auteur</label>
