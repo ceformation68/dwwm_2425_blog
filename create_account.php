0067c2e01b2d78cefb03dacfa83ccd0669e9afdd
Christel <christel.ehrhart@ce-soft.info>
2025-01-21 14:51:48 +0100

    Début créer un compte

---
+++ create_account.php
@@ -0,0 +1,25 @@
+<?php
+	// Variables d'affichage
+	$strTitle	= "Se connecter";
+	$strDesc	= "Page permettant de se connecter";
+
+	// Variables fonctionnelles
+	$strPage	= "login";
+	include_once("header.php");
+	
+	/** Si le formulaire est envoyé **/
+	// Récupère les infos du formulaire
+	
+	// Vérifications du formulaire => Affichage des erreurs
+	
+	// Créer un objet User
+	
+	// Appel une méthode dans le modèle, avec en paramètre l'objet	
+	
+	/** **/ 
+	
+?>
+	<!-- formulaire -->
+<?php
+	include_once("footer.php");
+?>
